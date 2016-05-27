2a737e11becaf48f8cbbfcb7ac3d786981087a7d
ianrahman <ian.alexander.rahman@gmail.com>
2016-05-27 09:43:02 -0400

    Removed instructions from project file

--- ios-prework-objc-assessment/AppDelegate.m
+++ ios-prework-objc-assessment/AppDelegate.m
@@ -17,62 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    // Declare a variable of type NSArray and assign it the return value from METHOD 1
-    
-    
-    // Declare a variable of type NSArray and assign it the return value from METHOD 2
-    
-    
-    // Declare a variable of type NSDictionary and assign it the return value of METHOD 3
-    
-    
-    // Use NSLog to print the contents of your dictionary to the console
-    
-    
     return YES;
 }
-
-/*
- 
- METHOD 1: generateArrayOfNumbers
- 
- Write a method to generate and return an array with the numbers 1 to 26
- 
- */
-
-// write method here
-
-
-
-
-/*
- 
- METHOD 2: generateArrayOfLetters
- 
- Write a method to generate and return an array with the letters a to z
- 
- */
-
-// write method here
-
-
-
-
-/*
- 
- METHOD 3: generateNumbersAndLettersDictionaryWithNumbers:letters:
- 
- Write a method that returns a dictionary. The method should have 
- two array parameters. Your numbers array should be passed in as 
- the first argument and your letters array should be passed in as 
- the second argument. Iterate over both arrays to create a 
- dictionary where the key is the number and the value is the letter.
- 
-*/
-
-// write method here
-
-
-
 
 @end
